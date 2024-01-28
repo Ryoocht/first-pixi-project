95b5357d07d6aee18b2cb47b9f68cdda6ce24584
Ryochi Tanaka <ryo1.t.zubu@gmail.com>
2024-01-28 23:15:07 +1000

    fix: base.ts webpack loader

--- webpack/base.ts
+++ webpack/base.ts
@@ -9,7 +9,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(ts|js)$/,
+        test: /\.js$/,
         excludes: /node_modules/,
         use: {
           loader: "babel-loader",
